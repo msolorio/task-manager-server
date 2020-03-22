75a4d16ba97c51e7110a405472b0bb193721b3d3
msolorio <msolorio789@gmail.com>
2020-03-22 12:25:19 -0700

    [wip] adding routes

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+'use strict';
+
+// TODO: REQUIRE PACKAGES
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
+
+// TODO: CONFIGURE PACKAGES
+app.use(morgan('common'));
 
 /*
 GET - "/" - GIVE US ALL DATA FOR BOARD
@@ -13,11 +22,44 @@ DELETE - "/list/:list_id" - REMOVE A LIST
 DELETE - "/list/:list_id/card/:card_id" - REMOVE A CARD FROM A LIST
 */
 
+// TODO: CONFIGURE ROUTES
+// TODO: MOVE ROUTES TO SEPARATE DIRECTORY AND REQUIRE IN
+
 app.get('/', (request, response) => {
-  response.send('data for board');
+  response.send('GET to /. get data for board');
 });
 
+// BOARD TITLE ROUTES
+app.put('/boardtitle', (request, response) => {
+  response.send('PUT to /boardtitle. update board title');
+});
 
+// LIST ROUTES
+
+// ADD LIST
+app.post('/list', (request, response) => {
+  response.send('POST to /list. add list');
+});
+
+// UPDATE LIST TITLE 
+app.put('/list/:listId/title', (request, response) => {
+  response.send('PUT to /list/:listId/title. update list title');
+});
+
+// CARD ROUTES
+
+// ADD CARD TO LIST
+app.post('/list/:listId/card', (request, response) => {
+  response.send('POST to /list/:listId/card. add card to list');
+});
+
+// TODO: ADD MORE ROUTES
+
+
+
+
+
+// TODO: CONNECT TO MONGO AND START SERVER
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}\n...you better go and catch it.`);
